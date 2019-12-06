749ff7cc38f57261d11d2cf16595d708b69c6ffb
Shinjice <Tdna84@gmail.com>
2019-12-06 16:16:14 +0100

    finished ganzenbord.v1

--- ganzenbord.v1/src/ganzenbordv1/ganzenbord.java
+++ ganzenbord.v1/src/ganzenbordv1/ganzenbord.java
@@ -1,9 +1,68 @@
 package ganzenbordv1;
 
-public class ganzenbord {
+import java.util.Random;
+import java.util.Scanner; 
 
-	public static void main(String[] args) {
-		System.out.print("Here we go!");
-	}
+public class ganzenbord {
+	
+	    private static Random random;
+
+	    public static void main(String[] args) {
+	        
+	    	Scanner pressKey = new Scanner(System.in);
+	    	
+	    	random = new Random();
+	        int position = 1;
+
+	        int diceRoll = g();
+
+	        while (position < 63)
+	        {
+	            position += diceRoll;        
+	            
+	            if (position==23)
+	            {
+	                System.out.println("Youve trown " + diceRoll + ", you are now at " + position + ", GAMEOVER!");              
+	                return;
+	                
+	            } else if (position == 25||position == 45){
+	            	
+	            	System.out.println("Youve trown " + diceRoll + ", you are now at " + position + ", RESTART!, press key to restart the GAME");
+	            	position=0;
+	            	String anyKey = pressKey.nextLine();
+	            
+	            } else if (position == 10||position == 20||position == 30||position == 40||position == 50||position == 60){
+	            	
+	            	int bonusJump = (position+diceRoll);
+	            	System.out.println("Youve trown " + diceRoll + ", you are now at " + position + ", BONUSJUMP!, you are now at " + bonusJump + ", press key to trow again");
+	            	position+=diceRoll;
+	            	String anyKey = pressKey.nextLine();
+	              
+	            } else {
+	            	
+	            	System.out.println("Youve trown " + diceRoll + ", you are now at " + position + ", no sweat, press key to trow again");
+	            	String anyKey = pressKey.nextLine();
+	            	
+	            }
+	            diceRoll = g();
+	        	}
+	           
+	        	System.out.println("You made it to position 63, YOUVE WON!");	     
+	    }
+	        
+	                
+
+	    public static int g()
+	    {
+	        int diceRoll = random.nextInt(6);
+	        diceRoll++;	        
+	        return diceRoll;
+	    }
 
 }
+	
+		
+		
+
+
+
